06b6c36eef461d249d636875a4563d9252a350c6
Chris Couzens <ccouzens@gmail.com>
2018-12-01 21:15:14 +0000

    Implement part 2 of day 1

--- day_1_frequency/src/lib.rs
+++ day_1_frequency/src/lib.rs
@@ -1,52 +1,120 @@
 pub fn frequency(input: &'static str) -> i32 {
-    let mut sum = 0;
-    let mut sign = true;
-    let mut value = 0;
-    for c in input.chars() {
-        match (c, c.to_digit(10)) {
-            ('+', None) => {
-                sign = true;
-                value = 0;
-            }
-            ('-', None) => {
-                sign = false;
-                value = 0;
-            }
-            (_, Some(digit)) => {
-                value = value * 10 + digit as i32;
+    numbers_iterator(input).sum()
+}
 
-                sum += if sign {
-                    value - value / 10
-                } else {
-                    value / 10 - value
-                };
-            }
-            _ => {}
+pub fn repeated_frequency(input: &'static str) -> i32 {
+    let mut seen = std::collections::HashSet::new();
+    let mut cumulative_sum = 0;
+    for num in numbers_iterator(input).cycle() {
+        if !seen.insert(cumulative_sum) {
+            return cumulative_sum;
         }
+        cumulative_sum += num;
     }
-    sum
+    panic!("should not reach");
 }
 
-#[test]
-fn worked_example() {
-    assert_eq!(frequency("+1, -2, +3, +1"), 3);
+fn numbers_iterator(input: &'static str) -> NumberIterator {
+    NumberIterator {
+        chars: input.chars().peekable(),
+    }
 }
 
-#[test]
-fn second_example() {
-    assert_eq!(frequency("+1, +1, +1"), 3);
-}
-#[test]
-fn thrid_example() {
-    assert_eq!(frequency("+1, +1, -2"), 0);
+#[derive(Clone)]
+struct NumberIterator<'a> {
+    chars: std::iter::Peekable<std::str::Chars<'a>>,
 }
 
-#[test]
-fn fourth_example() {
-    assert_eq!(frequency("-1, -2, -3"), -6);
+impl<'a> Iterator for NumberIterator<'a> {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut value = None;
+        let mut sign = None;
+        while let Some(c) = self.chars.by_ref().next() {
+            match (c, c.to_digit(10), sign) {
+                ('+', None, None) => {
+                    value = Some(0);
+                    sign = Some(true);
+                }
+                ('-', None, None) => {
+                    value = Some(0);
+                    sign = Some(false);
+                }
+                (_, Some(digit), Some(true)) => {
+                    value = value.map(|v| v * 10 + digit as i32);
+                }
+                (_, Some(digit), Some(false)) => {
+                    value = value.map(|v| v * 10 - digit as i32);
+                }
+                _ => {}
+            };
+            if self.chars.peek() == Some(&'+') || self.chars.peek() == Some(&'-') {
+                return value;
+            }
+        }
+        value
+    }
 }
 
-#[test]
-fn puzzle() {
-    assert_eq!(frequency(include_str!("../input.txt")), 493);
+#[cfg(test)]
+mod frequency {
+    use frequency;
+    #[test]
+    fn worked_example() {
+        assert_eq!(frequency("+1, -2, +3, +1"), 3);
+    }
+
+    #[test]
+    fn second_example() {
+        assert_eq!(frequency("+1, +1, +1"), 3);
+    }
+    #[test]
+    fn thrid_example() {
+        assert_eq!(frequency("+1, +1, -2"), 0);
+    }
+
+    #[test]
+    fn fourth_example() {
+        assert_eq!(frequency("-1, -2, -3"), -6);
+    }
+
+    #[test]
+    fn puzzle() {
+        assert_eq!(frequency(include_str!("../input.txt")), 493);
+    }
+}
+
+#[cfg(test)]
+mod repeated_frequency {
+    use repeated_frequency;
+    #[test]
+    fn worked_example() {
+        assert_eq!(repeated_frequency("+1, -2, +3, +1"), 2);
+    }
+
+    #[test]
+    fn second_example() {
+        assert_eq!(repeated_frequency("+1, -1"), 0);
+    }
+
+    #[test]
+    fn third_example() {
+        assert_eq!(repeated_frequency("+3, +3, +4, -2, -4"), 10);
+    }
+
+    #[test]
+    fn fourth_example() {
+        assert_eq!(repeated_frequency("-6, +3, +8, +5, -6"), 5);
+    }
+
+    #[test]
+    fn fifth_example() {
+        assert_eq!(repeated_frequency("+7, +7, -2, -7, -4"), 14);
+    }
+
+    #[test]
+    fn puzzle() {
+        assert_eq!(repeated_frequency(include_str!("../input.txt")), 413);
+    }
 }
