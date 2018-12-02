748fedd21d00860bed7fcfad31d0f7c2c0c9957e
Chris Couzens <ccouzens@gmail.com>
2018-12-02 19:42:32 +0000

    Refactor day 2's code

--- day_2_inventory_management_system/src/lib.rs
+++ day_2_inventory_management_system/src/lib.rs
@@ -4,14 +4,11 @@ use std::iter::FromIterator;
 pub fn checksum(list: &str) -> u32 {
     let (doubles, triples) = list.split_whitespace().map(id_analysis).fold(
         (0, 0),
-        |(mut doubles, mut triples), (double, triple)| {
-            if double {
-                doubles += 1
-            };
-            if triple {
-                triples += 1
-            };
-            (doubles, triples)
+        |(doubles, triples), (double, triple)| {
+            (
+                doubles + if double { 1 } else { 0 },
+                triples + if triple { 1 } else { 0 },
+            )
         },
     );
     doubles * triples
@@ -36,11 +33,11 @@ pub fn common_letters(list: &str) -> Option<String> {
 }
 
 fn id_analysis(id: &str) -> (bool, bool) {
-    let mut letter_counts: HashMap<char, u8> = HashMap::new();
+    let mut letter_counts = HashMap::new();
     for letter in id.chars() {
         *letter_counts.entry(letter).or_default() += 1;
     }
-    let counts: HashSet<u8> = HashSet::from_iter(letter_counts.values().cloned());
+    let counts: HashSet<u8> = HashSet::from_iter(letter_counts.drain().map(|kv| kv.1));
     (counts.contains(&2), counts.contains(&3))
 }
 
