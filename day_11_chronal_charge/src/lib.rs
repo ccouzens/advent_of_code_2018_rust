8b767d780d19c1a50eb9ce1d6669651239cb8d6e
Chris Couzens <ccouzens@gmail.com>
2018-12-23 21:20:50 +0000

    Implement day 11
    Unfortunately it is very slow.
    I may see if I can speed it up before going on.
    
    I suspect that it is because bigger square sizes are (probably)
    exponentially slower (until they become so big that you start having
    less of them).
    
    There are 297*297 3 by 3s. This comes to 793,881 cells to add together.
    There are 200*200 100 by 100s. This comes to 400,000,000 cells to add
    together.
    
    I suspect the approach I will take to fix this will be to not recalculate
    every square, but work out what gets subtracted from it, and what gets
    added to it.

--- day_11_chronal_charge/src/lib.rs
+++ day_11_chronal_charge/src/lib.rs
@@ -1,5 +1,19 @@
-pub fn highest_total_square(serial_number: u32) -> Option<(u32, u32)> {
-    FuelGrid { serial_number }.best_square()
+pub fn highest_total_square(serial_number: u32, square_size: u32) -> Option<(u32, u32)> {
+    FuelGrid { serial_number }.best_square(square_size)
+}
+
+pub fn highest_variable_square(serial_number: u32) -> Option<(u32, u32, u32)> {
+    (1..=300)
+        .flat_map(|s| highest_total_square(serial_number, s).map(|(x, y)| (x, y, s)))
+        .max_by_key(|&(x, y, s)| {
+            FuelSquare {
+                x,
+                y,
+                serial_number,
+                square_size: s,
+            }
+            .square_power_level()
+        })
 }
 
 struct FuelCell {
@@ -8,43 +22,65 @@ struct FuelCell {
     serial_number: u32,
 }
 
+struct FuelSquare {
+    x: u32,
+    y: u32,
+    serial_number: u32,
+    square_size: u32,
+}
+
 impl FuelCell {
     fn rack_id(&self) -> u32 {
         self.x + 10
     }
 
-    fn power_level(&self) -> i8 {
+    fn power_level(&self) -> i32 {
         let step_1 = self.rack_id();
         let step_2 = step_1 * self.y;
         let step_3 = step_2 + self.serial_number;
         let step_4 = step_3 * self.rack_id();
         let step_5 = (step_4 / 100) % 10;
-        step_5 as i8 - 5
+        step_5 as i32 - 5
+    }
+}
+
+impl FuelSquare {
+    fn square_power_level(&self) -> i32 {
+        self.iter().map(|n| n.power_level()).sum()
     }
 
-    fn square_power_level(&self) -> i8 {
-        self.square_iters().map(|n| n.power_level()).sum()
+    fn iter(&self) -> impl Iterator<Item = FuelCell> {
+        FuelSquareCellIterator {
+            serial_number: self.serial_number,
+            x: self.x,
+            y: self.y,
+            square_size: self.square_size,
+            iteration: 0,
+        }
     }
+}
 
-    fn square_iters<'a>(&'a self) -> impl Iterator<Item = FuelCell> + 'a {
-        let serial_number = self.serial_number;
-        [
-            (0, 0),
-            (0, 1),
-            (0, 2),
-            (1, 0),
-            (1, 1),
-            (1, 2),
-            (2, 0),
-            (2, 1),
-            (2, 2),
-        ]
-            .iter()
-            .map(move |&(x, y)| FuelCell {
-                x: x + self.x,
-                y: y + self.y,
-                serial_number,
-            })
+struct FuelSquareCellIterator {
+    serial_number: u32,
+    x: u32,
+    y: u32,
+    square_size: u32,
+    iteration: u32,
+}
+
+impl Iterator for FuelSquareCellIterator {
+    type Item = FuelCell;
+    fn next(&mut self) -> Option<FuelCell> {
+        if self.square_size * self.square_size == self.iteration {
+            return None;
+        }
+        let iteration = self.iteration;
+        self.iteration += 1;
+        Some(FuelCell {
+            x: self.x + iteration / self.square_size,
+            y: self.y + iteration % self.square_size,
+            serial_number: self.serial_number,
+        })
     }
 }
 
@@ -53,16 +89,16 @@ struct FuelGrid {
 }
 
 impl FuelGrid {
-    fn iter_squares(&self) -> impl Iterator<Item = FuelCell> {
+    fn iter_squares(&self, square_size: u32) -> impl Iterator<Item = FuelSquare> {
         FuelGridSquareIterator {
             serial_number: self.serial_number,
-            x: 1,
-            y: 1,
+            iteration: 0,
+            square_size,
         }
     }
 
-    fn best_square(&self) -> Option<(u32, u32)> {
-        self.iter_squares()
+    fn best_square(&self, square_size: u32) -> Option<(u32, u32)> {
+        self.iter_squares(square_size)
             .max_by_key(|fc| fc.square_power_level())
             .map(|fc| (fc.x, fc.y))
     }
@@ -70,54 +106,69 @@ impl FuelGrid {
 
 struct FuelGridSquareIterator {
     serial_number: u32,
-    x: u32,
-    y: u32,
+    iteration: u32,
+    square_size: u32,
 }
 
 impl Iterator for FuelGridSquareIterator {
-    type Item = FuelCell;
-    fn next(&mut self) -> Option<FuelCell> {
+    type Item = FuelSquare;
+    fn next(&mut self) -> Option<FuelSquare> {
         let serial_number = self.serial_number;
-        match (self.x, self.y) {
-            (298, 298) => None,
-            (298, y) => {
-                self.x = 1;
-                self.y += 1;
-                Some(FuelCell {
-                    x: 298,
-                    y,
-                    serial_number,
-                })
-            }
-            (x, y) => {
-                self.x += 1;
-                Some(FuelCell {
-                    x,
-                    y,
-                    serial_number,
-                })
-            }
+        let square_size = self.square_size;
+        let limit = 301 - square_size;
+        let x = 1 + self.iteration % limit;
+        let y = 1 + self.iteration / limit;
+        self.iteration += 1;
+        if y > limit {
+            None
+        } else {
+            Some(FuelSquare {
+                x,
+                y,
+                serial_number,
+                square_size,
+            })
         }
     }
 }
 
 #[cfg(test)]
-mod highest_total_square_tests {
+mod highest_3_by_3_total_square_tests {
     use highest_total_square;
 
     #[test]
     fn worked_example_1() {
-        assert_eq!(highest_total_square(18), Some((33, 45)));
+        assert_eq!(highest_total_square(18, 3), Some((33, 45)));
     }
 
     #[test]
     fn worked_example_2() {
-        assert_eq!(highest_total_square(42), Some((21, 61)));
+        assert_eq!(highest_total_square(42, 3), Some((21, 61)));
     }
 
     #[test]
     fn puzzle() {
-        assert_eq!(highest_total_square(5034), Some((235, 63)));
+        assert_eq!(highest_total_square(5034, 3), Some((235, 63)));
+    }
+}
+
+#[cfg(test)]
+mod highest_variable_square_size_tests {
+    use highest_variable_square;
+
+    #[test]
+    fn worked_example_1() {
+        assert_eq!(highest_variable_square(18), Some((90, 269, 16)));
+    }
+
+    #[test]
+    fn worked_example_2() {
+        assert_eq!(highest_variable_square(42), Some((232, 251, 12)));
+    }
+
+    #[test]
+    fn puzzle() {
+        assert_eq!(highest_variable_square(5034), Some((229, 251, 16)));
     }
 
 }
