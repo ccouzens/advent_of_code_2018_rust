3791d20b68f58f8fb7ebae36254b062b56746064
Chris Couzens <ccouzens@gmail.com>
2018-12-26 19:54:22 +0000

    Day 12, keep private methods private
    This alerted me to some dead code 👍.
    I prefer to only test the public interface as it avoids locking
    implementation details into the tests

--- day_12_subterranean_sustainability/src/lib.rs
+++ day_12_subterranean_sustainability/src/lib.rs
@@ -24,22 +24,22 @@ impl Cave {
         Ok(Cave { pots, rules })
     }
 
-    pub fn pots(&self) -> &BTreeSet<i64> {
+    fn pots(&self) -> &BTreeSet<i64> {
         &self.pots
     }
 
-    pub fn rules(&self) -> &HashSet<Rule> {
+    fn rules(&self) -> &HashSet<Rule> {
         &self.rules
+    }
+
+    fn pot_index_sum(&self) -> i64 {
+        self.pots().iter().cloned().sum()
     }
 
     pub fn generation_sum(mut self, generations: i64) -> i64 {
         let mut pot_counts = (None, None, None);
         for i in 0..generations {
-            pot_counts = (
-                pot_counts.1,
-                pot_counts.2,
-                Some(self.pots().iter().cloned().sum::<i64>()),
-            );
+            pot_counts = (pot_counts.1, pot_counts.2, Some(self.pot_index_sum()));
             if let (Some(a), Some(b), Some(c)) = pot_counts {
                 if c - b == b - a {
                     return (generations - i) * (b - a) + c;
@@ -47,10 +47,10 @@ impl Cave {
             }
             self = self.step();
         }
-        self.pots().iter().cloned().sum::<i64>()
+        self.pot_index_sum()
     }
 
-    pub fn step(mut self) -> Self {
+    fn step(mut self) -> Self {
         if let (Some(first), Some(last)) =
             (self.pots().iter().next(), self.pots().iter().rev().next())
         {
@@ -65,14 +65,6 @@ impl Cave {
             self.pots = new_pots;
         };
         self
-    }
-
-    pub fn fast_forward(self, generations: u64) -> Self {
-        let mut cave = self;
-        for _ in 0..generations {
-            cave = cave.step()
-        }
-        cave
     }
 }
 
@@ -106,55 +98,9 @@ named!(parse_rule< &str, Option<Rule>>,
 #[cfg(test)]
 mod worked_example_part_1 {
     use crate::Cave;
-    use std::collections::BTreeSet;
-    use std::collections::HashSet;
-    use std::iter::FromIterator;
 
     fn cave() -> Cave {
         Cave::try_new(include_str!("../worked_example.txt")).unwrap()
-    }
-
-    #[test]
-    fn first_generation() {
-        assert_eq!(
-            cave().pots(),
-            &BTreeSet::from_iter([0, 3, 5, 8, 9, 16, 17, 18, 22, 23, 24].into_iter().cloned())
-        );
-    }
-
-    #[test]
-    fn second_generation() {
-        assert_eq!(
-            cave().step().pots(),
-            &BTreeSet::from_iter([0, 4, 9, 15, 18, 21, 24].into_iter().cloned())
-        );
-    }
-
-    #[test]
-    fn rules() {
-        assert_eq!(
-            cave().rules(),
-            &HashSet::from_iter(
-                [
-                    (false, false, false, true, true),
-                    (false, false, true, false, false),
-                    (false, true, false, false, false),
-                    (false, true, false, true, false),
-                    (false, true, false, true, true),
-                    (false, true, true, false, false),
-                    (false, true, true, true, true),
-                    (true, false, true, false, true),
-                    (true, false, true, true, true),
-                    (true, true, false, true, false),
-                    (true, true, false, true, true),
-                    (true, true, true, false, false),
-                    (true, true, true, false, true),
-                    (true, true, true, true, false)
-                ]
-                .iter()
-                .cloned()
-            )
-        );
     }
 
     #[test]
