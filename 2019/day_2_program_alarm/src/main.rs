105fabe5d46fa753a43b6d33cfa6fa5ed2c98f91
Chris Couzens <ccouzens@gmail.com>
2019-12-02 20:34:40 +0000

    Day 2 2019: instruction pointer starts at 0

--- 2019/day_2_program_alarm/src/main.rs
+++ 2019/day_2_program_alarm/src/main.rs
@@ -1,24 +1,31 @@
 struct IntComputer {
-    program_counter: usize,
+    instruction_pointer: usize,
     memory: Vec<u32>,
 }
 
 impl IntComputer {
+    fn new(memory: Vec<u32>) -> Self {
+        Self {
+            memory,
+            instruction_pointer: 0,
+        }
+    }
+
     fn evaluate(&mut self) -> &Self {
         loop {
-            let opcode = self.memory[self.program_counter];
+            let opcode = self.memory[self.instruction_pointer];
             match opcode {
                 1 => {
-                    let read_address_1 = self.memory[self.program_counter + 1] as usize;
-                    let read_address_2 = self.memory[self.program_counter + 2] as usize;
-                    let store_address = self.memory[self.program_counter + 3] as usize;
+                    let read_address_1 = self.memory[self.instruction_pointer + 1] as usize;
+                    let read_address_2 = self.memory[self.instruction_pointer + 2] as usize;
+                    let store_address = self.memory[self.instruction_pointer + 3] as usize;
                     self.memory[store_address] =
                         self.memory[read_address_1] + self.memory[read_address_2];
                 }
                 2 => {
-                    let read_address_1 = self.memory[self.program_counter + 1] as usize;
-                    let read_address_2 = self.memory[self.program_counter + 2] as usize;
-                    let store_address = self.memory[self.program_counter + 3] as usize;
+                    let read_address_1 = self.memory[self.instruction_pointer + 1] as usize;
+                    let read_address_2 = self.memory[self.instruction_pointer + 2] as usize;
+                    let store_address = self.memory[self.instruction_pointer + 3] as usize;
                     self.memory[store_address] =
                         self.memory[read_address_1] * self.memory[read_address_2];
                 }
@@ -27,7 +34,7 @@ impl IntComputer {
                     panic!("Unexpected opcode");
                 }
             };
-            self.program_counter += 4;
+            self.instruction_pointer += 4;
         }
     }
 }
@@ -35,39 +42,21 @@ impl IntComputer {
 #[test]
 fn test_evaluate() {
     assert_eq!(
-        IntComputer {
-            program_counter: 0,
-            memory: vec!(1, 0, 0, 0, 99)
-        }
-        .evaluate()
-        .memory,
+        IntComputer::new(vec!(1, 0, 0, 0, 99)).evaluate().memory,
         vec!(2, 0, 0, 0, 99)
     );
     assert_eq!(
-        IntComputer {
-            program_counter: 0,
-            memory: vec!(2, 3, 0, 3, 99)
-        }
-        .evaluate()
-        .memory,
+        IntComputer::new(vec!(2, 3, 0, 3, 99)).evaluate().memory,
         vec!(2, 3, 0, 6, 99)
     );
     assert_eq!(
-        IntComputer {
-            program_counter: 0,
-            memory: vec!(2, 4, 4, 5, 99, 0)
-        }
-        .evaluate()
-        .memory,
+        IntComputer::new(vec!(2, 4, 4, 5, 99, 0)).evaluate().memory,
         vec!(2, 4, 4, 5, 99, 9801)
     );
     assert_eq!(
-        IntComputer {
-            program_counter: 0,
-            memory: vec!(1, 1, 1, 4, 99, 5, 6, 0, 99)
-        }
-        .evaluate()
-        .memory,
+        IntComputer::new(vec!(1, 1, 1, 4, 99, 5, 6, 0, 99))
+            .evaluate()
+            .memory,
         vec!(30, 1, 1, 4, 2, 5, 6, 0, 99)
     );
 }
@@ -81,10 +70,7 @@ fn main() -> Result<(), std::num::ParseIntError> {
         .collect::<Result<Vec<_>, _>>()?;
     memory[1] = 12;
     memory[2] = 2;
-    let mut computer = IntComputer {
-        memory,
-        program_counter: 0,
-    };
+    let mut computer = IntComputer::new(memory);
     computer.evaluate();
     println!("{}", computer.memory[0]);
 
