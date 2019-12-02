3da1357ec0e691de77d32a7461725567f35a545a
Chris Couzens <ccouzens@gmail.com>
2019-12-02 20:56:24 +0000

    Solve day 2 part 2 2019
    I'm surprised I didn't get any panics from exceeding the bounds of my
    memory. I suppose the bounds checking will come on a later day.

--- 2019/day_2_program_alarm/src/main.rs
+++ 2019/day_2_program_alarm/src/main.rs
@@ -11,68 +11,74 @@ impl IntComputer {
         }
     }
 
-    fn evaluate(&mut self) -> &Self {
+    fn follow(&mut self, offset: usize) -> &mut u32 {
+        let address = self.memory[self.instruction_pointer + offset] as usize;
+        &mut self.memory[address]
+    }
+
+    fn evaluate(&mut self) {
         loop {
             let opcode = self.memory[self.instruction_pointer];
             match opcode {
                 1 => {
-                    let read_address_1 = self.memory[self.instruction_pointer + 1] as usize;
-                    let read_address_2 = self.memory[self.instruction_pointer + 2] as usize;
-                    let store_address = self.memory[self.instruction_pointer + 3] as usize;
-                    self.memory[store_address] =
-                        self.memory[read_address_1] + self.memory[read_address_2];
+                    *self.follow(3) = *self.follow(1) + *self.follow(2);
+                    self.instruction_pointer += 4;
                 }
                 2 => {
-                    let read_address_1 = self.memory[self.instruction_pointer + 1] as usize;
-                    let read_address_2 = self.memory[self.instruction_pointer + 2] as usize;
-                    let store_address = self.memory[self.instruction_pointer + 3] as usize;
-                    self.memory[store_address] =
-                        self.memory[read_address_1] * self.memory[read_address_2];
+                    *self.follow(3) = *self.follow(1) * *self.follow(2);
+                    self.instruction_pointer += 4;
                 }
-                99 => return self,
+                99 => return,
                 _ => {
                     panic!("Unexpected opcode");
                 }
             };
-            self.instruction_pointer += 4;
         }
     }
 }
 
+fn evaluate(memory: Vec<u32>) -> Vec<u32> {
+    let mut computer = IntComputer::new(memory);
+    computer.evaluate();
+    computer.memory
+}
+
 #[test]
 fn test_evaluate() {
+    assert_eq!(evaluate(vec!(1, 0, 0, 0, 99)), vec!(2, 0, 0, 0, 99));
+    assert_eq!(evaluate(vec!(2, 3, 0, 3, 99)), vec!(2, 3, 0, 6, 99));
     assert_eq!(
-        IntComputer::new(vec!(1, 0, 0, 0, 99)).evaluate().memory,
-        vec!(2, 0, 0, 0, 99)
-    );
-    assert_eq!(
-        IntComputer::new(vec!(2, 3, 0, 3, 99)).evaluate().memory,
-        vec!(2, 3, 0, 6, 99)
-    );
-    assert_eq!(
-        IntComputer::new(vec!(2, 4, 4, 5, 99, 0)).evaluate().memory,
+        evaluate(vec!(2, 4, 4, 5, 99, 0)),
         vec!(2, 4, 4, 5, 99, 9801)
     );
     assert_eq!(
-        IntComputer::new(vec!(1, 1, 1, 4, 99, 5, 6, 0, 99))
-            .evaluate()
-            .memory,
+        evaluate(vec!(1, 1, 1, 4, 99, 5, 6, 0, 99)),
         vec!(30, 1, 1, 4, 2, 5, 6, 0, 99)
     );
 }
 
 fn main() -> Result<(), std::num::ParseIntError> {
     let input = include_str!("../input");
-    let mut memory = input
+    let program = input
         .split(|c: char| !c.is_ascii_digit())
         .filter(|v| !v.is_empty())
         .map(|v| v.parse())
         .collect::<Result<Vec<_>, _>>()?;
-    memory[1] = 12;
-    memory[2] = 2;
-    let mut computer = IntComputer::new(memory);
-    computer.evaluate();
-    println!("{}", computer.memory[0]);
-
+    for (noun, verb) in (0..=99)
+        .flat_map(|noun| (0..=99).map(move |verb| (noun, verb)))
+        .filter(|&(noun, verb)| {
+            let mut memory = program.clone();
+            memory[1] = noun;
+            memory[2] = verb;
+            evaluate(memory)[0] == 19690720
+        })
+    {
+        println!(
+            "noun: {}, verb: {}, answer: {}",
+            noun,
+            verb,
+            100 * noun + verb
+        );
+    }
     Ok(())
 }
